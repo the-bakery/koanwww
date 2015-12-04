d87887456c84d22136f18b3ab80b51920ee4f706
Chris Johnson <chris@cjohnson.io>
2015-12-03 23:42:47 -0800

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('webapp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngNewRouter', 'mgcrea.ngStrap', 'toastr']);
+
+})();
