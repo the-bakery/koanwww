65350b9f4d656840b4e1eb1558f99f81d02b4f1d
Matt Vana <errantspark@gmail.com>
2016-02-19 00:41:23 -0800

    v2

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -4,7 +4,8 @@
   angular
     .module('webapp')
     .config(routerConfig)
-    .controller('RouterController', RouterController);
+    .controller('RouterController', RouterController)
+    .controller('HeaderController', HeaderController);
 
   /** @ngInject */
   function routerConfig($componentLoaderProvider) {
@@ -20,8 +21,19 @@
       { path: '/team', component: 'team' },
       { path: '/join', component: 'join' },
       { path: '/contact', component: 'contact' },
-      { path: '/blog', component: 'blog' }
+      { path: '/blog', component: 'blog' },
+      { path: '/collective', component: 'collective' },
+      { path: '/consulting', component: 'consulting' },
+      { path: '/about', component: 'about' }
     ]);
+  }
+  /** question for chris: where should this actually be 
+   *http://stackoverflow.com/questions/16199418/how-do-i-implement-the-bootstrap-navbar-active-class-with-angular-js */
+  function HeaderController($scope, $location) 
+  { 
+    $scope.isActive = function (viewLocation) { 
+      return viewLocation === $location.path();
+    };
   }
 
 })();
